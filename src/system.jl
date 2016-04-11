00d108dec9db10a953f91fc3962d504e38624bfd
Mike Innes <mike.j.innes@gmail.com>
2016-04-11 12:00:52 +0100

    0.5 deprecation

--- src/system.jl
+++ src/system.jl
@@ -8,7 +8,7 @@ export render, setdisplay, unsetdisplay, getdisplay, current_input, Media, @medi
 distance(S, T) =
   !(S <: T) ? Inf :
   S == T ? 0. :
-  1 + distance(super(S), T)
+  1 + distance(supertype(S), T)
 
 nearest(T, U, V) =
   distance(T, U) < distance(T, V) ? U : V
